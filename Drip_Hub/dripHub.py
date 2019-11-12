7fbe585d235a5f2484c3dc67ee24fd1071a4d6dd
kthenshaw <kthenshaw@gmail.com>
2019-11-12 16:43:37 -0500

    System integration

--- Drip_Hub/dripHub.py
+++ Drip_Hub/dripHub.py
@@ -4,18 +4,21 @@
 # Date: November 11, 2019
 
 import paho.mqtt.client as mqtt
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 import time
+import os
 
 import requests
 from bs4 import BeautifulSoup
 import json
+import netifaces as ni
 
 class Drip():
 	def __init__(self):
 
 		self.clientName    = "Drip" #Define the script's name for when it appears on the Server
-		self.serverAddress = "18.21.156.233" #Define the address of the server
+		ni.ifaddresses('wlan0')
+		self.serverAddress = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr'] #Define the address of the server
 		self.mqttClient    = mqtt.Client(self.clientName) #Define the client
 
 		#Callback function is called when script is connected to mqtt server
@@ -26,19 +29,19 @@ class Drip():
 		self.mqttClient.message_callback_add('Drip/startup',self.on_message_startup)
 		self.mqttClient.on_message = self.on_message
 
-		self.mqttClient.connect(self.serverAddress)
 		#Subscribe to everything that starts with Drip/
+		self.mqttClient.connect(self.serverAddress)
 		self.mqttClient.subscribe("Drip/#")
 
 		#Defining location variables
 		self.latitude  = None
 		self.longitude = None
 		
-		#Defining global variables that the app subscribes to
+		#Defining global variables that the app subscribes to - THESE ARE DUMMY VARIABLES
 		self.temperature = [20,0]
 		self.state       = 1
 		self.battery     = 90
-		self.danger      = 'moderate'
+		self.danger      = 'Moderate'
 
 		#Defining whether or not setup has been completed
 		self.setup = False
@@ -48,17 +51,23 @@ class Drip():
 
 		#### VARIABLES - SHOULD BE CHANGED ######
 		#### VARIABLES REGARDING RELAY DEVICE ###
-		self.off_interval       = 3 #seconds. modulate pump 3 seconds off, 1 second on
+		self.off_interval       = 4 #seconds. modulate pump 3 seconds off, 1 second on
 		self.on_interval        = 1 #seconds. modulate pump 3 seconds off, 1 second on
-		self.num_pump_intervals = 10 #of times modulate_pump function will turn pump on an off. may need to be variable going forward.
-		self.check_interval     = 1 #minutes. how often the script will check the current temperature and decide whether or not to modulate the pump.
+		self.num_pump_intervals = 2 #of times modulate_pump function will turn pump on an off. may need to be variable going forward.
+		self.check_interval     = 5 #minutes. how often the script will check the current temperature and decide whether or not to modulate the pump.
 		self.threshold_temp     = 32 #degrees. threshold below which we start drip system
 
+		## LOGIC VARIABLES
+		self.level1Temp = 32
+		self.level2Temp = 10
+		self.level3Temp = 0
+		
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(self.channel, GPIO.OUT)
 
 		#This causes the script to loop forever
-		self.mqttClient.loop_forever()
+		self.mqttClient.loop_start()
+		self.run_cycle()
 
 	def on_connect(self, client, userdata, flags, rc):
 		'''
@@ -66,12 +75,7 @@ class Drip():
 		Prints connected to verify
 		'''
 		print('Connected')
-		if not self.setup:
-			pass
-		else:
-			print('Run cycle')
-			self.run_cycle()
-
+		
 	def on_message(self, client, userdata, msg):
 		'''
 		Prints message for any message not received through Drip/ topics
@@ -105,7 +109,7 @@ class Drip():
 		client.publish("Drip/State", "State,{}".format(self.state))
 		client.publish("Drip/Temperature","Temp,{}".format(self.temperature[0]))
 		client.publish("Drip/Battery", "Bat,{}".format(self.battery))
-		client.publish("Drip/Danger", "Danger,{}".formate(self.danger))
+		client.publish("Drip/Danger", "Danger,{}".format(self.danger))
 		self.setup = True
 	
 	def get_weather_data(self):
@@ -114,57 +118,78 @@ class Drip():
 		Returns a list of temperatures fro the next 150 hours
 		'''
 		#define url that takes latitiude and longitude variables
-	    url='https://api.weather.gov/points/'+str(self.latitude)+','+str(self.longitude)
-	    initial=requests.get(url)
-	    html=BeautifulSoup(initial.content,features="html.parser")
+		url='https://api.weather.gov/points/'+str(self.latitude)+','+str(self.longitude)
+		initial=requests.get(url)
+		html=BeautifulSoup(initial.content,features="html.parser")
 
-	    dict_one=json.loads(html.text)
-	    url=dict_one["properties"]["forecastHourly"]
-	    #do again but with new url
-	    initial=requests.get(url)
-	    html=BeautifulSoup(initial.content,features="html.parser")
-	    dict_one=json.loads(html.text)
-	    props=dict_one["properties"]
-	    temps=[]
+		dict_one=json.loads(html.text)
+		url=dict_one["properties"]["forecastHourly"]
+		#do again but with new url
+		initial=requests.get(url)
+		html=BeautifulSoup(initial.content,features="html.parser")
+		dict_one=json.loads(html.text)
+		props=dict_one["properties"]
+		temps=[]
 
+		for period in props["periods"]:
+	        	temps.append(period["temperature"])
 
-	    for period in props["periods"]:
-	        temps.append(period["temperature"])
-	    return temps
+		currentTemp = temps[0]
+		if currentTemp>self.level1Temp:
+			self.state=0
+			self.danger = "None"
+		elif currentTemp<self.level1Temp:
+			self.state=1
+			self.danger = "Low"
+		elif currentTemp<self.level2Temp:
+			self.state=2
+			self.danger = "Moderate"
+		elif currentTemp<self.level3Temp:		
+			self.state=3
+			self.danger = "High"
+		return temps
 
 	def pump_on(self, pin):
-	    GPIO.output(pin, GPIO.HIGH) #turn pump on
-	    print("Pump on")
+		os.system('./test_code_rf/CC1101/raspi/TX_Go -a1 -r2 -i1000 -t0 -c1 -f434 -m250')
+		GPIO.output(pin, GPIO.HIGH) #turn pump on
+		print("Pump on")
     
 	def pump_off(self, pin):
-	    GPIO.output(pin, GPIO.LOW)
-	    print("Pump off")
+		os.system('./test_code_rf/CC1101/raspi/TX_Stop -a1 -r2 -i1000 -t0 -c1 -f434 -m250')
+		GPIO.output(pin, GPIO.LOW)
+		print("Pump off")
+		
 	    
 	def modulate_pump(self):
-	    for i in range(self.num_pump_intervals):
-
-	    	self.pump_on(self.channel)
-	    	time.sleep(self.on_interval)
-	    	pump_off(self.channel)
-	    	time.sleep(self.off_interval)
-	    print("number of intervals completed: ", self.num_pump_intervals)
+		for i in range(self.num_pump_intervals):
+			time_cycled = time.time()
+			self.pump_on(self.channel)
+			time.sleep(self.on_interval)
+			self.pump_off(self.channel)
+			time.sleep(self.off_interval)
+		print("number of intervals completed: ", self.num_pump_intervals)
 
 	def on_off_threshold(self):
-		if self.get_weather_data(self.latitude, self.longitude) > self.threshold_temp:
-			return "No freezing"
+		if self.temperature[0] > self.threshold_temp:
+			print("No freezing")
 		else:
 			return self.modulate_pump()
 
 	def run_cycle(self):
-		starttime = time.time()
 		while True:
-			print(self.on_off_threshold())
-			#The code sleeps/pauses until a minute has passed by
-			time.sleep(abs((60*self.check_interval - ((time.time() - starttime) % 60.0))))
-			# while abs(60*self.check_interval - ((time.time() - starttime))) > 0:
-			# 	self.mqttClient.message_callback_add('Drip/location',self.on_message_location)
-			# 	self.mqttClient.message_callback_add('Drip/startup',self.on_message_startup)
-			# 	self.mqttClient.subscribe("Drip/#")
+			print("While True")
+			self.on_off_threshold()
+			try:
+				self.temperature=self.get_weather_data()
+			except:
+				pass
+			starttime = time.time()
+		#The code sleeps/pauses until a minute has passed by
+		#time.sleep(abs((60*self.check_interval - ((time.time() - starttime) % 60.0))))
+			while 60*self.check_interval - ((time.time() - starttime)) > 0:
+			#while 2 - ((time.time() - starttime)) > 0:
+				pass
+				
 
 def main():
 	'''
