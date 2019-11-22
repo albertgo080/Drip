7212a38e58e5e31d079aa3482bd027f503ea1d6c
Isaac Perper <iperper@mit.edu>
2019-11-21 21:55:12 -0500

    Refactoring code.  Made pump and MQTT client classes

--- Drip_Hub/drip_manual.py
+++ Drip_Hub/drip_manual.py
@@ -3,236 +3,135 @@
 # Author: MIT 2.009 Purple Team
 # Date: November 11, 2019
 
-import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import time
 import os
+from Pump import Pump
+from DripClient import DripClient
 
 import requests
-from bs4 import BeautifulSoup
 import json
-import netifaces as ni
 
-class Drip():
-	def __init__(self):
+import argparse
 
-		self.clientName    = "Drip" #Define the script's name for when it appears on the Server
-		ni.ifaddresses('wlan0')
-		self.serverAddress = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr'] #Define the address of the server
-		self.mqttClient    = mqtt.Client(self.clientName) #Define the client
+import logging
 
-		#Callback function is called when script is connected to mqtt server
-		self.mqttClient.on_connect = self.on_connect
+class DripHub():
+    def __init__(self, client_name="Drip", off_interval=4, \
+                    on_interval=4, num_pump_intervals=2, check_interval=1, threshold_temp=32):
 
-		#Establishing and calling callback functions for when messages from different topics are received
-		self.mqttClient.message_callback_add('Drip/location',self.on_message_location)
-		self.mqttClient.message_callback_add('Drip/startup',self.on_message_startup)
-		self.mqttClient.message_callback_add('Drip/manual',self.on_message_manual)
-		self.mqttClient.on_message = self.on_message
+         # ID of system as appears on server  
+        self.client_name = client_name 
+        
+        #### VARIABLES - SHOULD BE CHANGED ######
+        #### VARIABLES REGARDING RELAY DEVICE ###
+        self.off_interval       = off_interval # seconds. modulate pump 4 seconds off
+        self.on_interval        = on_interval # seconds. modulate pump 4 seconds on
+        self.num_pump_intervals = num_pump_intervals # of times modulate_pump function will turn pump on an off. 
+        self.check_interval     = check_interval # minutes. how often the script will check the current temperature and decide whether or not to modulate the pump.
+        self.threshold_temp     = threshold_temp #degrees. threshold below which we start drip system
+        
+        self.pump_channel       = 17 # for pump relay
+        self.led_green          = 16 # for status relay
 
-		#Subscribe to everything that starts with Drip/
-		self.mqttClient.connect(self.serverAddress)
-		self.mqttClient.subscribe("Drip/#")
-
-		#Defining location variables
-		self.latitude  = None
-		self.longitude = None
-		
-		#Defining global variables that the app subscribes to - THESE ARE DUMMY VARIABLES
-		self.temperature = [50,0]
-		self.state       = 0
-		self.battery     = 90
-		self.danger      = 'None'
-
-		#Defining whether or not setup has been completed
-		self.setup = False
-
-		#Defining GPIO pin for the relay
-		self.channel  = 17
-		self.ledGreen = 16
-
-		#### VARIABLES - SHOULD BE CHANGED ######
-		#### VARIABLES REGARDING RELAY DEVICE ###
-		self.off_interval       = 4 #seconds. modulate pump 3 seconds off, 1 second on
-		self.on_interval        = 4 #seconds. modulate pump 3 seconds off, 1 second on
-		self.num_pump_intervals = 2 #of times modulate_pump function will turn pump on an off. 
-		self.check_interval     = 1 #minutes. how often the script will check the current temperature and decide whether or not to modulate the pump.
-		self.threshold_temp     = 32 #degrees. threshold below which we start drip system
-
-		## LOGIC VARIABLES
-		self.level1Temp = 32
-		self.level2Temp = 10
-		self.level3Temp = 0
-
-		self.manual = False
-		
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(self.channel, GPIO.OUT)
-		GPIO.setup(self.ledGreen, GPIO.OUT)
-
-		#This causes the script to loop forever
-		self.mqttClient.loop_start()
-		self.run_cycle()
-
-	def on_connect(self, client, userdata, flags, rc):
-		'''
-		Callback function for when the script is connected to the server
-		Prints connected to verify
-		'''
-		print('Connected')
-		
-	def on_message(self, client, userdata, msg):
-		'''
-		Prints message for any message not received through Drip/ topics
-		'''
-		message = msg.payload.decode(encoding='UTF-8')
-		print(message)
-
-	def on_message_location(self, client, userdata, msg):
-		'''
-		Callback function for Drip/location topics
-		Gets the temperature based on the location that is specified
-		'''
-		message = msg.payload.decode(encoding='UTF-8')
-		print(message)
-		#split on the comma because the message in the form of "latitude, longitude"
-		location=message.split(",")
-
-		self.latitude = location[0]
-		self.longitude = location[1]
-
-		self.temperature=self.get_weather_data()
-		print(self.temperature[0])
-		
-	def on_message_startup(self, client, userdata, msg):
-		'''
-		Callback function for Drip/startup topic
-		Publishes State, Temperature, Battery, and Danger information to the respective topics
-		This function is called everythime someone opens the Drip dashboard in the app
-		Also is called when someone hits the refresh button on the app
-		'''
-		client.publish("Drip/State", "State,{}".format(self.state))
-		client.publish("Drip/Temperature","Temp,{}".format(self.temperature[0]))
-		client.publish("Drip/Battery", "Bat,{}".format(self.battery))
-		client.publish("Drip/Danger", "Danger,{}".format(self.danger))
-		self.setup = True
-
-	def on_message_manual(self, client, userdata, msg):
-		'''
-		Callback function for Drip/manual topic
-		Sets the self.manual variabl as true and thus opens the valve
-		'''
-		message = msg.payload.decode(encoding='UTF-8')
-		if message == "on":
-			self.manual = True
-		else:
-			self.manual = False
-		print("Manual Settings:", self.manual)
-	
-	def get_weather_data(self):
-		'''
-		Gets weather data from the location specified by latittude and longitude
-		Returns a list of temperatures fro the next 150 hours
-		'''
-		#define url that takes latitiude and longitude variables
-		url='https://api.weather.gov/points/'+str(self.latitude)+','+str(self.longitude)
-		initial=requests.get(url)
-		html=BeautifulSoup(initial.content,features="html.parser")
-
-		dict_one=json.loads(html.text)
-		url=dict_one["properties"]["forecastHourly"]
-		#do again but with new url
-		initial=requests.get(url)
-		html=BeautifulSoup(initial.content,features="html.parser")
-		dict_one=json.loads(html.text)
-		props=dict_one["properties"]
-		temps=[]
-
-		for period in props["periods"]:
-	        	temps.append(period["temperature"])
-
-		currentTemp = temps[0]
-		if currentTemp>self.level1Temp:
-			self.state=0
-			self.danger = "None"
-		elif currentTemp<self.level1Temp:
-			self.state=1
-			self.danger = "Low"
-		elif currentTemp<self.level2Temp:
-			self.state=2
-			self.danger = "Moderate"
-		elif currentTemp<self.level3Temp:		
-			self.state=3
-			self.danger = "High"
-		return temps
-
-	def pump_on(self, pin):
-		#GPIO.output(self.ledGreen, GPIO.HIGH) #turn green led off
-		os.system('./test_code_rf/CC1101/raspi/TX_Go -a1 -r2 -i1000 -t0 -c1 -f434 -m250')
-		GPIO.output(pin, GPIO.HIGH) #turn pump on
-		GPIO.output(self.ledGreen, GPIO.HIGH) #turn green led on
-		print("Pump on")
-		return
+        #########################################
     
-	def pump_off(self, pin):
-		#GPIO.output(self.ledGreen, GPIO.LOW) #turn green led off
-		os.system('./test_code_rf/CC1101/raspi/TX_Stop -a1 -r2 -i1000 -t0 -c1 -f434 -m250')
-		GPIO.output(pin, GPIO.LOW)
-		GPIO.output(self.ledGreen, GPIO.LOW) #turn green led off
-		print("Pump off")
-		return
-		
-	    
-	def modulate_pump(self):
-		for i in range(self.num_pump_intervals):
-			time_cycled = time.time()
-			self.pump_on(self.channel)
-			while self.on_interval - ((time.time() - time_cycled)) > 0 and not self.manual:
-				pass
-			time_cycled2 = time.time()
-			self.pump_off(self.channel)
-			while self.on_interval - ((time.time() - time_cycled2)) > 0 and not self.manual:
-				pass
-		print("number of intervals completed: ", self.num_pump_intervals)
-		return
+        # Set RPi GPIO pins to blink LED
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.led_green, GPIO.OUT)
 
-	def on_off_threshold(self):
-		if self.temperature[0] > self.threshold_temp:
-			self.pump_off(self.channel)
-			print("No freezing")
-		else:
-			return self.modulate_pump()
 
-	def run_cycle(self):
-		while True:
-			print("While True")
-			if self.manual:
-				self.pump_on(self.channel)
-				self.state = 1
-				self.danger = "Moderate"
-				print("manual on")
-			else:
-				self.on_off_threshold()
-			try:
-				self.temperature=self.get_weather_data()
-			except:
-				pass
-			starttime = time.time()
-			#The code sleeps/pauses until a minute has passed by
-			while 5*self.check_interval - ((time.time() - starttime)) > 0 and not self.manual:
-				pass
-				
+        # Create MQQT and weather object
+        self.client = DripClient(self.client_name)
 
-def main():
-	'''
-	Main function that calls the class
-	'''
-	print('main')
-	foo = Drip()
+        # Create Pump object
+        self.pump = Pump(self.pump_channel)
+
+    def pump_on(self, pin):
+        GPIO.output(self.led_green, GPIO.HIGH) #turn green led on
+        self.pump.pump_on()
+        logger.debug("Pump and LED on")
+        return
+    
+    def pump_off(self, pin):
+        self.pump.pump_off()
+        GPIO.output(self.led_green, GPIO.LOW) #turn green led off
+        logger.debug("Pump and LED off")
+        return
+        
+    def modulate_pump(self):
+        for i in range(self.num_pump_intervals):
+            time_cycled = time.time()
+            self.pump.pump_on()
+            while self.on_interval - ((time.time() - time_cycled)) > 0 and not self.client.manual:
+                pass
+            time_cycled2 = time.time()
+            self.pump.pump_off()
+            while self.on_interval - ((time.time() - time_cycled2)) > 0 and not self.client.manual:
+                pass
+        logger.debug("number of intervals completed: %i", self.num_pump_intervals)
+        return
+
+    def on_off_threshold(self):
+        if self.client.temperature[0] > self.threshold_temp:
+            self.pump.pump_off()
+            logger.debug("No freezing")
+        else:
+            return self.modulate_pump()
+
+    def run_cycle(self):
+        logger.debug("In run_cycle")
+        if self.client.manual:
+            self.pump.pump_on()
+            self.client.state = 1
+            self.client.danger = "Moderate"
+            logger.debug("manual on")
+        else:
+            self.on_off_threshold()
+        try:
+            self.temperature=self.client.get_weather_data()
+        except:
+            pass
+        starttime = time.time()
+
+        # The code sleeps/pauses until a minute has passed by
+        while 5*self.check_interval - ((time.time() - starttime)) > 0 and not self.client.manual:
+            pass
+                
+
+# def main():
+#     '''
+#     Main function that calls the class
+#     '''
+#     logger.info('In main')
+#     foo = DripHub()
 
 if __name__ == '__main__':
-	main()
+    parser = argparse.ArgumentParser(description="Run the drip systems")
+    parser.add_argument('off_interval', default=4, type=float, help='seconds that pump modulates off')
+    parser.add_argument('on_interval', default=4, type=float, help='seconds that pump modulates on')
+    parser.add_argument('num_intervals', default=2, type=int, help='Number of times pump modulates off')
+    parser.add_argument('check_interval', default=1, type=float, help='How often in minutes hub checks for new temperatures')
+    parser.add_argument('threshold_temp', default=32, type=float, help='degress Fahrenheight Threshold below which drip starts')
 
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    Hub = DripHub("Drip", args.off_interval, args.on_interval, args.num_intervals, args.check_interval, args.threshold_temp)
+    
+    # run drip until it is interrupted
+    try:
+        while True:
+            Hub.run_cycle()
+    except KeyboardInterrupt:
+        pass
+
+    # turn off pump before exiting
+    Hub.pump_off()
+
+    logger.debug("Exited cleanly")
 
 
 
